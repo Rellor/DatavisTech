e4a32b7e97d0093532d4d5dc855d8a7abf688e7c
Rellor <basderoller@gmail.com>
2021-10-28 10:58:08 +0200

    toLowerCase functie toegevoegd. Nog niet opgeschoond.

--- app.js
+++ app.js
@@ -8,10 +8,19 @@ const datafile = require('./tech-track-dataset.json');
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
+function showdata() {
+  const vraag = "Wat wil je worden als je groot bent?"
 
-app.get('/', (req, res) => res.render('index.ejs', {
-datading: datafile
-}))
+  const uitkomst = datafile.map(antwoord =>
+   antwoord[vraag].toLowerCase()
+ )
+ console.log(uitkomst);
+ app.get('/', (req, res) => res.render('index.ejs', {
+ datading: uitkomst
+ }))
+}
+
+showdata()
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
